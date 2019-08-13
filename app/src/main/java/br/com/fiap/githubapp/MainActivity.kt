39b6a2da35cd7803e34852f02830f65b24bbc766
Logon Aluno <logonaluno@local.com>
2019-08-12 23:01:05 -0300

    Okay

--- app/src/main/java/br/com/fiap/githubapp/MainActivity.kt
+++ app/src/main/java/br/com/fiap/githubapp/MainActivity.kt
@@ -4,76 +4,53 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
-import br.com.fiap.githubapp.api.GithubService
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import br.com.fiap.githubapp.model.Usuario
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_main.*
-import retrofit2.Call
-import retrofit2.Callback
-import retrofit2.Response
-import retrofit2.Retrofit
-import retrofit2.converter.gson.GsonConverterFactory
-import com.facebook.stetho.okhttp3.StethoInterceptor
 import kotlinx.android.synthetic.main.include_loading.*
-import okhttp3.OkHttpClient
-
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mainViewModel: MainViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        mainViewModel = ViewModelProviders.of(this)
+            .get(MainViewModel::class.java)
+
         btBuscar.setOnClickListener {
-
-            showLoading()
-
-            val retrofit = Retrofit.Builder()
-                .baseUrl("https://api.github.com/")
-                .addConverterFactory(GsonConverterFactory.create())
-                .client(
-                    OkHttpClient.Builder()
-                        .addNetworkInterceptor(StethoInterceptor())
-                        .build()
-                )
-                .build()
-
-            val service = retrofit.create(GithubService::class.java)
-
-            service.pequisar(inputUsuario.text.toString())
-                .enqueue(object : Callback<Usuario> {
-
-                    override fun onFailure(call: Call<Usuario>, t: Throwable) {
-                        hideLoading()
-
-                        Toast.makeText(
-                            this@MainActivity, t.message, Toast.LENGTH_LONG
-                        ).show()
-                    }
-
-                    override fun onResponse(call: Call<Usuario>, response: Response<Usuario>) {
-                        hideLoading()
-
-                        if (response.isSuccessful) {
-
-                            val usuario = response.body()
-
-                            tvNomeUsuario.text = usuario?.nome
-
-                            Picasso.get()
-                                .load(usuario?.avatarURL)
-                                .into(ivUsuario)
-
-
-
-                        } else {
-                            Toast.makeText(
-                                this@MainActivity, "Não foi possivel realizar a operação", Toast.LENGTH_LONG
-                            ).show()
-                        }
-                    }
-                })
+            mainViewModel.pesquisar(inputUsuario.text.toString())
         }
+
+        registerObserver()
+    }
+
+    private fun registerObserver() {
+        mainViewModel.usuarioResponse.observe(this, Observer {
+            setUsuario(it)
+        })
+        mainViewModel.loading.observe(this, Observer {
+            if(it == true)
+                showLoading()
+            else
+                hideLoading()
+        })
+        mainViewModel.messageError.observe(this, Observer {
+            if(it != "")
+                Toast.makeText(this, it, Toast.LENGTH_LONG).show()
+        })
+    }
+
+    private fun setUsuario(usuario: Usuario?) {
+        tvNomeUsuario.text = usuario?.nome
+
+        Picasso.get()
+            .load(usuario?.avatarURL)
+            .into(ivUsuario)
     }
 
     private fun showLoading() {
@@ -83,6 +60,4 @@ class MainActivity : AppCompatActivity() {
     private fun hideLoading() {
         containerLoading.visibility = View.GONE
     }
-
-
 }
